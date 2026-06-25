45713e1a81c60a713e01e3d63b129292d75485d7
Gabriel David <g28571240@gmail.com>
2026-06-25 15:39:35 +0200

    Corrigiendo errores

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 //Conexion apuntando a la carpeta 'data'
+const path = requiere('path');
 const dbPath = path.join(__dirname, 'data', 'database.sqlite');
 const db = new sqlite3.Database(dbPath, (err) => {
     // ... resto de codigo inicializacion
