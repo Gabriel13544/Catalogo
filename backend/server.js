77bd411eb9761125ef63341d121f60ffb77b559b
Gabriel David <g28571240@gmail.com>
2026-06-27 11:20:59 -0400

    Fix console log message for SQLite connection

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,30 @@
-const path = require ('path')
-const sqlite3 = require ('sqlite3')
-const express = require ('express')
-const cors = require ('cors')
+const path = require('path');
+const sqlite3 = require('sqlite3');
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const dbPath = path.join(__dirname, 'data', 'database.sqlite');
 
 const db = new sqlite3.Database(dbPath, (err) => {
-	if (err) {
-		console.error('Error al conectar a SQLite:', err.message);
-	} else{
-		console.error('Conectado con exito a la base de datos SQLite.');
-	}
+    if (err) {
+        console.error('Error al conectar a SQLite:', err.message);
+    } else {
+        console.log('Conectado con éxito a la base de datos SQLite.');
+    }
 });
 
-//Configuraciones basicas para tu API
+// Configuraciones basicas para tu API
 app.use(cors());
 app.use(express.json());
 
-//Ruta de pruba para saber si el backend response
+// Ruta de prueba para saber si el backend responde
 app.get('/', (req, res) => {
-	res.send('El servidor de catalogo esta funcionando correctamente.');
+    res.send('El servidor de catalogo esta funcionando correctamente.');
 });
 
-//Escuchar en el puerto que te de render automaticamente
+// Escuchar en el puerto que te de render automaticamente
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
-	console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
